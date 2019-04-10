5fddc14af7205d181ebd3366fa2d9e03b52d25a3
jmirs <yongseokj2@gmail.com>
2019-04-10 19:18:09 +0900

    Add source file

---
+++ src/yshell.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <pwd.h>
+
+#define MAX_BUF_LEN 100
+
+void get_current_dir(char *usr_name, char *shell_dir) {
+	/* get current dir. */
+	char *usr_dir = (char *)malloc(sizeof(char)*MAX_BUF_LEN);
+	getcwd(usr_dir, MAX_BUF_LEN);
+
+	/* cut off "home/" */
+	strcpy(shell_dir, strstr(usr_dir, usr_name));
+
+	free(usr_dir);
+}
+
+int main() {
+	int should_run = 1;    /* flag to determine when to exit program*/
+
+	// get user name
+	uid_t id = getuid();
+	struct passwd *user = getpwuid(id);
+	char *usr_name = user->pw_name;
+
+	while(should_run) {
+		char *shell_dir = (char *)malloc(sizeof(char)*MAX_BUF_LEN);
+		get_current_dir(usr_name, shell_dir);
+		printf("YShell::%s$ ", shell_dir);
+		fflush(stdout);
+
+		free(shell_dir);
+	}
+
+	return 0;
+}
