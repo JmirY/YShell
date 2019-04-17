d1053e71b0f05dfaa94e81666d849ce6532a8c1e
jmirs <yongseokj2@gmail.com>
2019-04-17 17:34:54 +0900

    Add -a option in ls

--- src/cmd_ls.c
+++ src/cmd_ls.c
@@ -2,19 +2,33 @@
 #include "yshell.h"
 #define MAX_BUF_LEN 200
 
-extern void cmd_ls(char *tokens[]) {
+void cmd_ls(char *tokens[]) {
 	char *current_dir = (char *)malloc(sizeof(char)*MAX_BUF_LEN);
 	DIR *dir = NULL;
 	struct dirent *entry = NULL;
+	char flg_show_all = 0;  /* flag to determine either show files start with '.' */
 
+	if(tokens[0] != NULL) {
+		if(!strcmp(tokens[0], "-a"))
+			flg_show_all = 1;
+		else {
+			printf("ERROR@cmd_ls(): Wrong option!\n");
+			return;
+		}
+	}
 	getcwd(current_dir, MAX_BUF_LEN);
 	dir = opendir(current_dir);
 	if(dir == NULL) {
-		printf("ERROR\n");
+		printf("ERROR@cmd_ls()\n");
 		return;
 	}
+
 	entry = readdir(dir);
 	while(entry != NULL) {
+		if(flg_show_all == 0 && entry->d_name[0] == '.') {
+			entry = readdir(dir);
+			continue;
+		}
 		printf("%s\t", entry->d_name);
 		entry = readdir(dir);
 	}
